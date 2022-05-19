ad4b1e604fa306677cc43b8a2e432f6cff18579f
SoniaRoss <srross99@gmail.com>
2022-05-18 23:06:02 -0400

    Initial commint-EF database first with scaffolding

---
+++ CreateMovieApi/Controllers/MovieController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CreateMovieApi.Data;
+using CreateMovieApi.Models;
+
+namespace CreateMovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly RossMovieDbContext _context;
+
+        public MovieController(RossMovieDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movie
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+          if (_context.Movies == null)
+          {
+              return NotFound();
+          }
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/Movie/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+          if (_context.Movies == null)
+          {
+              return NotFound();
+          }
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // PUT: api/Movie/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Movie
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+          if (_context.Movies == null)
+          {
+              return Problem("Entity set 'RossMovieDbContext.Movies'  is null.");
+          }
+            _context.Movies.Add(movie);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MovieExists(movie.MovieId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMovie", new { id = movie.MovieId }, movie);
+        }
+
+        // DELETE: api/Movie/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if (_context.Movies == null)
+            {
+                return NotFound();
+            }
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return (_context.Movies?.Any(e => e.MovieId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CreateMovieApi/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreateMovieApi.Models
+{
+    public partial class Movie
+    {
+        public int MovieId { get; set; }
+        public string? MovieTitle { get; set; }
+        public string? Genre { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
